8810119f75103c68d0bbb3574ddd20bac533d192
haochunchang <b03b02014@ntu.edu.tw>
2017-10-14 11:35:48 +0800

    unit_test --> sequence data, new utils

---
+++ hw1/sequence_data.py
@@ -0,0 +1,25 @@
+import utils
+import os
+import pandas as pd
+import numpy as np
+
+datadir='data/'
+feature='fbank'
+
+xtrain = utils.load_data(os.path.join(datadir, '{}'.format(feature)))
+y_train = pd.read_csv(os.path.join(datadir, 'label', 'train.lab'), 
+                              header=None, names=['id', 'label'])
+
+merged = xtrain.merge(y_train, how='left')
+steps = 10
+utils.get_sequence(merged, steps, feature)
+utils.combine_data(feature, steps)
+
+datadir='data/'
+feature='mfcc'
+
+xtrain = utils.load_data(os.path.join(datadir, '{}'.format(feature)))
+merged = xtrain.merge(y_train, how='left')
+steps = 10
+utils.get_sequence(merged, steps, feature)
+utils.combine_data(feature, steps)
--- hw1/utils.py
+++ hw1/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+import glob
 
 def load_data(path, flag='train'):
 
@@ -56,7 +57,7 @@ def combine_phone_seq(res):
     new_result = res.groupby(new_idx).apply(lambda x: x['pred'].str.cat(sep=','))
     return new_result
 
-def get_sequence(merged, steps):
+def get_sequence(merged, steps, feature):
     '''
     Preprocess features, avoiding cross-sentences steps
     For each sentence start, pad with zeros, not previous sentence
@@ -68,17 +69,36 @@ def get_sequence(merged, steps):
     merged.index = pd.MultiIndex.from_tuples([tuple(k.split('_')) for k in merged['id']]) 
     merged = merged.drop(['id', 'label'], axis=1)
 
-    padding = np.array([[0 for i in range(len(merged['feature'].values[0]))] for j in range(steps)])
-    x_train = np.empty([1, steps, len(merged['feature'].values[0])])
-    print('Initialize empty x_train: {}'.format(x_train.shape))
+    padding = np.zeros((steps // 2, len(merged['feature'].values[0])))
+    i = 0
     for person, new_df in merged.groupby(level=0):
+        x_train = np.empty((1, steps, len(merged['feature'].values[0])))
         for sentence, fea in new_df.groupby(level=1):
             frames = np.append(padding, np.array([i for i in fea['feature'].values]), axis=0)
-            x_train = np.append(x_train, np.array([frames[i:i+steps, :] for i in range(frames.shape[0]-steps)]), axis=0)
-    x_train = np.delete(x_train, 0, axis=0)
-    return x_train
+            frames = np.append(frames, padding, axis=0)
+            x_train = np.append(x_train, np.array([frames[i-steps//2:i+steps//2, :] 
+                                                   for i in range(steps//2, frames.shape[0]-steps//2)]), axis=0)
+        x_train = np.delete(x_train, (0), axis=0)
+        print(i, x_train.shape)
+        np.save('./data/{}/{}_steps{}_{}'.format(feature, feature, steps, i), x_train)
+        i += 1
+
+def combine_data(feature, steps):
+    paths = glob.glob(os.path.join('data', feature, '{}_steps{}*'.format(feature, steps)))
+    if feature == 'fbank':
+        x_train = np.empty((1, steps, 69))
+    else:
+        x_train = np.empty((1, steps, 39))
+
+    for i in range(462):
+        path = os.path.join('data', feature, '{}_steps{}_{}.npy'.format(feature, steps, i))
+        tmp = np.load(path)
+        print('{} loaded'.format(path))
+        x_train = np.append(x_train, tmp, axis=0)
+    x_train = np.delete(x_train, (0), axis=0)     
+    print(x_train.shape)
+    np.save('{}_all_steps{}'.format(feature, steps), x_train)
 
 if __name__ == "__main__":
-    path = os.path.join('./data')
-    test_result = pd.read_csv('test_result.csv')
-    trim(test_result, path)
+    combine_data('fbank', 20)
+    combine_data('mfcc', 20)
