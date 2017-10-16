943d3602179d1e3254f38db32c8395c334ca0781
haochunchang <b03b02014@ntu.edu.tw>
2017-10-17 01:23:49 +0800

    New preprocessing

--- hw1/sequence_data.py
+++ hw1/sequence_data.py
@@ -6,23 +6,77 @@ import numpy as np
 datadir='data/'
 feature='fbank'
 
-#xtrain = utils.load_data(os.path.join(datadir, '{}'.format(feature)))
+xtrain = utils.load_data(os.path.join(datadir, '{}'.format(feature)))
 y_train = pd.read_csv(os.path.join(datadir, 'label', 'train.lab'), 
                               header=None, names=['id', 'label'])
 
-#merged = xtrain.merge(y_train, how='left')
-steps = 6
-#utils.get_sequence(merged, steps, feature)
-
-#x_test = utils.load_data(os.path.join(datadir, feature), flag='test')
-#utils.get_test_sequence(x_test, steps, feature)
-datadir='data/'
-feature='mfcc'
-
-xtrain = utils.load_data(os.path.join(datadir, '{}'.format(feature)))
 merged = xtrain.merge(y_train, how='left')
-steps = 6
-utils.get_sequence(merged, steps, feature)
-x_test = utils.load_data(os.path.join(datadir, feature), flag='test')
-utils.get_test_sequence(x_test, steps, feature)
+merged.index = pd.MultiIndex.from_tuples([tuple(k.split('_')) for k in merged['id']])
+
+sents = []
+labels = []
+for person, new_df in merged.groupby(level=0):
+    for sentence, fea in new_df.groupby(level=1):
+        label = np.array([i for i in fea['label'].values])
+        label = label.reshape(label.shape[0], 1)
+        lpadding = np.array(['sil' for i in range(777-label.shape[0])])
+        lpadding = lpadding.reshape((lpadding.shape[0], 1))
+        
+        frames = np.array([i for i in fea['feature'].values])
+        padding = np.zeros((777-frames.shape[0], frames.shape[1]))
+        sents.append(np.append(frames, padding, axis=0))
+        labels.append(np.append(label, lpadding, axis=0))
+
+new = np.array([i for i in sents])
+np.save('./data/{}/sents'.format(feature), new)
+
+merged = utils.load_data(os.path.join(datadir, feature), flag='test')
+merged.index = pd.MultiIndex.from_tuples([tuple(k.split('_')) for k in merged['id']])
+sents = []
+for person, new_df in merged.groupby(level=0):
+    for sentence, fea in new_df.groupby(level=1):       
+        frames = np.array([i for i in fea['feature'].values])
+        padding = np.zeros((777-frames.shape[0], frames.shape[1]))
+        sents.append(np.append(frames, padding, axis=0))
+new = np.array([i for i in sents])
+np.save('./data/{}/test_sents'.format(feature), new)
+
+
+feature='mfcc'
+xtrain = utils.load_data(os.path.join(datadir, '{}'.format(feature)))
+y_train = pd.read_csv(os.path.join(datadir, 'label', 'train.lab'), 
+                              header=None, names=['id', 'label'])
+
+merged = xtrain.merge(y_train, how='left')
+merged.index = pd.MultiIndex.from_tuples([tuple(k.split('_')) for k in merged['id']])
+
+sents = []
+labels = []
+for person, new_df in merged.groupby(level=0):
+    for sentence, fea in new_df.groupby(level=1):
+        label = np.array([i for i in fea['label'].values])
+        label = label.reshape(label.shape[0], 1)
+        lpadding = np.array(['sil' for i in range(777-label.shape[0])])
+        lpadding = lpadding.reshape((lpadding.shape[0], 1))
+        
+        frames = np.array([i for i in fea['feature'].values])
+        padding = np.zeros((777-frames.shape[0], frames.shape[1]))
+        sents.append(np.append(frames, padding, axis=0))
+        labels.append(np.append(label, lpadding, axis=0))
+
+new = np.array([i for i in sents])
+np.save('./data/{}/sents'.format(feature), new)
+
+merged = utils.load_data(os.path.join(datadir, feature), flag='test')
+merged.index = pd.MultiIndex.from_tuples([tuple(k.split('_')) for k in merged['id']])
+sents = []
+for person, new_df in merged.groupby(level=0):
+    for sentence, fea in new_df.groupby(level=1):       
+        frames = np.array([i for i in fea['feature'].values])
+        padding = np.zeros((777-frames.shape[0], frames.shape[1]))
+        sents.append(np.append(frames, padding, axis=0))
+new = np.array([i for i in sents])
+np.save('./data/{}/test_sents'.format(feature), new)
+
+
 
