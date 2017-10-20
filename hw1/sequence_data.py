ddd5b7a43b3bc88d5dd0848f8a9c58961a7ccb6a
haochunchang <b03b02014@ntu.edu.tw>
2017-10-21 01:50:21 +0800

    ADD sent end

--- hw1/sequence_data.py
+++ hw1/sequence_data.py
@@ -13,86 +13,86 @@ merged.index = pd.MultiIndex.from_tuples([tuple(k.split('_')) for k in merged['i
 
 sents = []
 labels = []
+sent_end = []
 for person, new_df in merged.groupby(level=0):
     for sentence, fea in new_df.groupby(level=1):
-        # Label padding with <START> and <END>
         label = np.array([i for i in fea['label'].values])
-        label = np.append(np.array(['<START>']), label, axis=0)
-        label = np.append(label, np.array(['<END>']), axis=0)
         label = label.reshape(label.shape[0], 1)
-        lpadding = np.array(['sil' for i in range(779-label.shape[0])])
+        lpadding = np.array(['sil' for i in range(777-label.shape[0])])
         lpadding = lpadding.reshape((lpadding.shape[0], 1))
-        labels.append(np.append(label, lpadding, axis=0))
-
-        # Feature mean subtraction and padding
+        
         frames = np.array([i for i in fea['feature'].values])
-        frames = np.transpose(np.transpose(frames) - np.transpose(frames).mean(axis=0))
-        padding = np.zeros((779-frames.shape[0], frames.shape[1]))
+        padding = np.zeros((777-frames.shape[0], frames.shape[1]))
         sents.append(np.append(frames, padding, axis=0))
-    
+        labels.append(np.append(label, lpadding, axis=0))
+        sent_end.append(frames.shape[0])
+
+np.save('./data/{}/sent_ends'.format(feature), np.array(sent_end))
 np.save('./data/{}/sents_labels'.format(feature), np.array([i for i in labels]))
 
 new = np.array([i for i in sents])
 np.save('./data/{}/sents'.format(feature), new)
 
-# fbank testing data
 merged = utils.load_data(os.path.join(datadir, feature), flag='test')
 merged.index = pd.MultiIndex.from_tuples([tuple(k.split('_')) for k in merged['id']])
 sents = []
+sent_end = []
 for person, new_df in merged.groupby(level=0):
     for sentence, fea in new_df.groupby(level=1):       
-        # Feature mean subtraction and padding
         frames = np.array([i for i in fea['feature'].values])
-        frames = np.transpose(np.transpose(frames) - np.transpose(frames).mean(axis=0))
-        padding = np.zeros((779-frames.shape[0], frames.shape[1]))
+        padding = np.zeros((777-frames.shape[0], frames.shape[1]))
         sents.append(np.append(frames, padding, axis=0))
+        sent_end.append(frames.shape[0])
+
+np.save('./data/{}/test_sent_ends'.format(feature), np.array(sent_end))
 
 new = np.array([i for i in sents])
 np.save('./data/{}/test_sents'.format(feature), new)
 
-#===========================================================================
+#======================================================================
 feature='mfcc'
 
 xtrain = utils.load_data(os.path.join(datadir, '{}'.format(feature)))
 y_train = pd.read_csv(os.path.join(datadir, 'label', 'train.lab'), 
                               header=None, names=['id', 'label'])
+
 merged = xtrain.merge(y_train, how='left')
 merged.index = pd.MultiIndex.from_tuples([tuple(k.split('_')) for k in merged['id']])
 
 sents = []
 labels = []
+sent_end = []
 for person, new_df in merged.groupby(level=0):
     for sentence, fea in new_df.groupby(level=1):
-        # Label padding with <START> and <END>
         label = np.array([i for i in fea['label'].values])
-        label = np.append(np.array(['<START>']), label, axis=0)
-        label = np.append(label, np.array(['<END>']), axis=0)
         label = label.reshape(label.shape[0], 1)
-        lpadding = np.array(['sil' for i in range(779-label.shape[0])])
+        lpadding = np.array(['sil' for i in range(777-label.shape[0])])
         lpadding = lpadding.reshape((lpadding.shape[0], 1))
-        labels.append(np.append(label, lpadding, axis=0))
-
-        # Feature mean subtraction and padding
+        
         frames = np.array([i for i in fea['feature'].values])
-        frames = np.transpose(np.transpose(frames) - np.transpose(frames).mean(axis=0))
-        padding = np.zeros((779-frames.shape[0], frames.shape[1]))
+        padding = np.zeros((777-frames.shape[0], frames.shape[1]))
         sents.append(np.append(frames, padding, axis=0))
+        labels.append(np.append(label, lpadding, axis=0))
+        sent_end.append(frames.shape[0])
+
+np.save('./data/{}/sent_ends'.format(feature), np.array(sent_end))
 
 np.save('./data/{}/sents_labels'.format(feature), np.array([i for i in labels]))
 new = np.array([i for i in sents])
 np.save('./data/{}/sents'.format(feature), new)
 
-# MFCC testing data
 merged = utils.load_data(os.path.join(datadir, feature), flag='test')
 merged.index = pd.MultiIndex.from_tuples([tuple(k.split('_')) for k in merged['id']])
 sents = []
+sent_end = []
 for person, new_df in merged.groupby(level=0):
     for sentence, fea in new_df.groupby(level=1):       
-        # Feature mean subtraction and padding
         frames = np.array([i for i in fea['feature'].values])
-        frames = np.transpose(np.transpose(frames) - np.transpose(frames).mean(axis=0))
-        padding = np.zeros((779-frames.shape[0], frames.shape[1]))
+        padding = np.zeros((777-frames.shape[0], frames.shape[1]))
         sents.append(np.append(frames, padding, axis=0))
+        sent_end.append(frames.shape[0])
+
+np.save('./data/{}/test_sent_ends'.format(feature), np.array(sent_end))
 
 new = np.array([i for i in sents])
 np.save('./data/{}/test_sents'.format(feature), new)
