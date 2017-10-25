f6f09f621876c6e96d05e5e4ac7b4380f7a12f4a
hcc_on_azure <b03b02014@ntu.edu.tw>
2017-10-25 03:18:05 +0000

    Load best model

--- hw1/best.py
+++ hw1/best.py
@@ -1,45 +1,42 @@
 import utils
-import os, sys
+import os, sys, pickle
 import pandas as pd
+import numpy as np
 import argparse
 
+def main(datadir, outfilepath, model='best'):
 
-## Load best.h5, best_model.json, best_(f)label_map.pkl to produce best submission
-def main(datadir, outfilepath, model='rnn', feature='fbank'):
-
-    if model == 'rnn':
-        import model_rnn as md
-    elif model == 'cnn':
-        import model_cnn as md 
+    import model_cnn as md 
 
     # Load in training data
-    y_train = pd.read_csv(os.path.join(datadir, 'label', 'train.lab'), 
-                              header=None, names=['id', 'label'])
-    # Use both kinds of features, averaging their model predictions
-    if feature == 'both':          
-        clf_f = md.load_pretrained(model_name='{}_f'.format(model))
-        clf_m = md.load_pretrained(model_name='{}_m'.format(model))
-        # Testing
-        x_test_f = utils.load_data(os.path.join(datadir, 'fbank'), flag='test')
-        x_test_m = utils.load_data(os.path.join(datadir, 'mfcc'), flag='test')
-        result_f = md.primary_test(clf_f, x_test_f, model_name='{}_f'.format(model))
-        result_m = md.primary_test(clf_m, x_test_m, model_name='{}_m'.format(model))
-        y_pred = (result_f + result_m) / 2
-        with open('{}_flabel_map.pkl'.format(model), 'rb') as lm:
-            label_map = pickle.load(lm)
-    
-        pred = label_map.inverse_transform(y_pred, 0.5)
-        result = pd.DataFrame()
-        result['id'] = idx
-        result['pred'] = pred
+    #y_train = pd.read_csv(os.path.join(datadir, 'label', 'train.lab'),
+	#    header=None, names=['id', 'label'])
 
-    # using only one of either features
-    else:
-        clf = md.load_pretrained(model_name=model)
-        # Testing
-        x_test = utils.load_data(os.path.join(datadir, '{}'.format(feature)), flag='test')
-        result = md.test(clf, x_test, model_name=model)
+    clf = md.load_pretrained(model_name=model)
+    # Testing
+    x_test_f = utils.load_data(os.path.join(datadir, 'fbank'), flag='test')
+    x_test_m = utils.load_data(os.path.join(datadir, 'mfcc'), flag='test')
+    utils.get_test_sequence(x_test_f, x_test_m, save_all=False)
+    y_pred, idx = md.test(clf, x_test_f, model_name=model)
    
+    #with open('predict_proba.pkl', 'wb') as p:
+    #    pickle.dump((y_pred, idx), p)
+ 
+    threshold = 0.6
+    with open('label_map.pkl', 'rb') as lm:
+        label_map = pickle.load(lm)
+
+    new_pred = []
+    for label in y_pred:
+        tmp = label_map.inverse_transform(label, threshold)
+        new_pred.append(tmp)
+
+    new_pred = np.array(new_pred)
+    result = pd.DataFrame()
+    result['id'] = idx
+    result['pred'] = new_pred.reshape((new_pred.shape[0]*new_pred.shape[1], 1))
+    #result.to_csv('{}prime.csv'.format(model), index=False)
+    
     # Post-processing for submission 
     result = utils.combine_phone_seq(result)
     result = utils.trim(result, datadir)
@@ -47,13 +44,14 @@ def main(datadir, outfilepath, model='rnn', feature='fbank'):
     sub['id'] = result.index
     sub['phone_sequence'] = result.values
     sub.to_csv(outfilepath, index=False)  
-    
+
 if __name__ == "__main__":
-   
+
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", help="Data Directory")   
     parser.add_argument("outpath", help="output file name / path") 
-    parser.add_argument("feature", type=str, choices=['fbank', 'mfcc', 'both'], help="Select input feature", default='fbank')
+    #parser.add_argument("-t", "--train", help="Train model from scratch", action='store_true')
+    #parser.add_argument("model", type=str, choices=['rnn', 'cnn', 'concat'], help="Select model: rnn or +cnn", default='rnn')
     args = parser.parse_args()
 
-    main(args.dir, args.outpath, feature=args.feature, model='best')
+    main(args.dir, args.outpath)
