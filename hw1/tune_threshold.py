8865379ffe50ece5b37a0abadced68d7c22f3932
haochunchang <b03b02014@ntu.edu.tw>
2017-10-27 23:27:53 +0800

    Try to tune proportion and threshold

--- hw1/tune_threshold.py
+++ hw1/tune_threshold.py
@@ -6,20 +6,33 @@ import editdistance
 from bayes_opt import BayesianOptimization
 from keras.models import model_from_json
 
-def train_distance(threshold):
+def train_distance(x, y, threshold):
 
+    datadir = './data'
+    
     model_name = 'best'  
-    (y_pred, idx) = pd.read_pickle('{}_train_proba.pkl'.format(model_name)) 
+    (best_pred, idx) = pd.read_pickle('{}_train_proba.pkl'.format(model_name)) 
+    model_name = 'cnn'  
+    (cnn_pred, idx) = pd.read_pickle('{}_train_proba.pkl'.format(model_name)) 
+    model_name = 'rnn'  
+    (rnn_pred, idx) = pd.read_pickle('{}_train_proba.pkl'.format(model_name)) 
+    
+    y_pred = rnn_pred * x + cnn_pred * y + best_pred * (1-x-y)
+
     with open('label_map.pkl', 'rb') as lm:
         label_map = pickle.load(lm)
 
     # Hand cut threshold
+    y_pred[y_pred < threshold] = 0
+
+    new_pred = []
+    for label in y_pred:
+        tmp = label_map.inverse_transform(label, 0.5)
+        new_pred.append(tmp)
+    new_pred = np.array(new_pred).reshape(3696*777)
     result = pd.DataFrame()
     result['id'] = idx
-    result['pred'] = y_pred.reshape((y_pred.shape[0]*y_pred.shape[1], 48))
-    result['pred'][result['pred'] < threshold] = 0
-
-    result['pred'] = label_map.inverse_transform(result['pred'].values, 0.5)
+    result['pred'] = new_pred
 
     result = utils.combine_phone_seq(result)
     result = utils.trim(result, datadir)
@@ -38,10 +51,10 @@ def train_distance(threshold):
     return sum(distance)/len(distance)
     
 def find_threshold():
-    #with open('predict_proba.pkl', 'rb') as p:
 
-    bo = BayesianOptimization(train_distance, {'threshold': [0.1, 1.0]})
-    bo.explore({'threshold': [0.1, 1.0]})
+    bo = BayesianOptimization(train_distance, {'x': [0.1, 0.5], 'y':[0.25, 0.9], 'threshold': [0.1, 1.0]})
+    bo.explore({'threshold': [0.1, 1.0], 'x': [0.1, 0.5], 'y':[0.25, 0.9]})
+    bo.initialize({'x': 0.2, 'y': 0.4, 'threshold': 0.5})
     bo.maximize(init_points=5, n_iter=15, kappa=2)
     print(bo.res['max'])
 
