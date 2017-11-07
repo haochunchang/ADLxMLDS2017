bd82e28ebb3610f0c2bf02e30d2435b5ae3b8348
haochunchang <b03b02014@ntu.edu.tw>
2017-11-08 00:46:06 +0800

    chmod .sh

--- hw2/hw2_seq2seq.sh
+++ hw2/hw2_seq2seq.sh
@@ -3,5 +3,10 @@
 # $2: test data output filename 
 # $3: peer review output filename 
 #Ex: ./hw2_seq2seq.sh myData  sample_output_testset.txt sample_output_peer_review.txt
-
+if [ ! -d "./models" ]; then
+  mkdir ./models
+  wget --directory-prefix=models https://gitlab.com/haochunchang/ADLxMLDS2017_model_archive/raw/master/basic_models-499.data-00000-of-00001?private_token=6hvzcU4AsJbfNUGE7Ymc
+  wget --directory-prefix=models https://gitlab.com/haochunchang/ADLxMLDS2017_model_archive/raw/master/basic_models-499.index?private_token=6hvzcU4AsJbfNUGE7Ymc
+  wget --directory-prefix=models https://gitlab.com/haochunchang/ADLxMLDS2017_model_archive/raw/master/basic_models-499.meta?private_token=6hvzcU4AsJbfNUGE7Ymc
+fi
 python test_seq2seq.py $1 ./models/basic_models-499 $2 $3
--- hw2/hw2_special.sh
+++ hw2/hw2_special.sh
@@ -1,3 +1,9 @@
 #  produce corresponding captions.
 # $1: data dir, $2: output_file
+if [ ! -d "./models" ]; then
+  mkdir ./models
+  wget --directory-prefix=models https://gitlab.com/haochunchang/ADLxMLDS2017_model_archive/raw/master/basic_models-499.data-00000-of-00001?private_token=6hvzcU4AsJbfNUGE7Ymc
+  wget --directory-prefix=models https://gitlab.com/haochunchang/ADLxMLDS2017_model_archive/raw/master/basic_models-499.index?private_token=6hvzcU4AsJbfNUGE7Ymc
+  wget --directory-prefix=models https://gitlab.com/haochunchang/ADLxMLDS2017_model_archive/raw/master/basic_models-499.meta?private_token=6hvzcU4AsJbfNUGE7Ymc
+fi
 python special_test.py $1 ./models/basic_models-499 $2
