a4c497875decdfe407b5538356f91bd3eb828c03
hcc_on_azure <b03b02014@ntu.edu.tw>
2017-11-07 15:54:05 +0000

    add special mission

--- hw2/hw2_seq2seq.sh
+++ hw2/hw2_seq2seq.sh
@@ -4,4 +4,4 @@
 # $3: peer review output filename 
 #Ex: ./hw2_seq2seq.sh myData  sample_output_testset.txt sample_output_peer_review.txt
 
-
+python test_seq2seq.py $1 ./models/basic_models-499 $2 $3
--- hw2/hw2_special.sh
+++ hw2/hw2_special.sh
@@ -1,3 +1,3 @@
 #  produce corresponding captions.
 # $1: data dir, $2: output_file
-
+python special_test.py $1 ./models/basic_models-499 $2
