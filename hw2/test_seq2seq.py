e2963e484c7dbee4308debeaa01a96a2984581a9
haochunchang <b03b02014@ntu.edu.tw>
2017-11-08 01:16:15 +0800

    add peer review

--- hw2/test_seq2seq.py
+++ hw2/test_seq2seq.py
@@ -7,7 +7,7 @@ import VCG_model as VCG
 from os import listdir
 from os.path import join, isfile
 
-def test(model_path='./', datadir='./data', outfilepath='captions.csv'):
+def test(model_path='./', datadir='./data', outfilepath='captions.csv', peer='peer.csv'):
 
     dim_image = 4096
     dim_hidden = 512
@@ -16,6 +16,7 @@ def test(model_path='./', datadir='./data', outfilepath='captions.csv'):
     n_caption_lstm_step = 20
     n_frame_step = 80
     #===================================================
+    # Testing data set
     video_path = join(datadir, 'testing_data', 'feat')
     test_videos = [f for f in listdir(video_path) if isfile(join(video_path, f))]
 
@@ -74,7 +75,45 @@ def test(model_path='./', datadir='./data', outfilepath='captions.csv'):
     captions['id'] = index
     captions['captions'] = generated_sentences
     captions.to_csv(outfilepath, index=False, header=False)
+    #=====================================================================================
+    # Testing peer review
+    video_path = join(datadir, 'peer_review', 'feat')
+    test_videos = [f for f in listdir(video_path) if isfile(join(video_path, f))]
+    
+    generated_sentences = []
+    index = []
+    for idx, video in enumerate(test_videos):
+        print('video =>', video)
 
+        video_feat_path = join(datadir, 'peer_review', 'feat', video)
+        video_feat = np.load(video_feat_path)[None,...]
+        if video_feat.shape[1] == n_frame_step:
+            video_mask = np.ones((video_feat.shape[0], video_feat.shape[1]))
+        else:
+            continue
+
+        generated_word_index = sess.run(caption_tf, feed_dict={video_tf: video_feat, video_mask_tf: video_mask})
+        generated_words = ixtoword[generated_word_index]
+        punctuation = np.argmax(np.array(generated_words) == '<pad>')
+        if punctuation > 0:
+            generated_words = generated_words[:punctuation] 
+        
+        generated_sentence = ' '.join(generated_words)
+        generated_sentence = generated_sentence.replace('<pad>', '')        
+        generated_sentence = generated_sentence.replace('<unk>', '')       
+        generated_sentence = generated_sentence.replace('<bos> ', '')
+        generated_sentence = generated_sentence.replace(' <eos>', '')
+
+        print('generated_sentence =>', generated_sentence)
+ 
+        generated_sentences.append(generated_sentence)
+        index.append('.'.join(video.split('.')[:-1]))
+
+    captions = pd.DataFrame()
+    captions['id'] = index
+    captions['captions'] = generated_sentences
+    captions.to_csv(peer, index=False, header=False)
+ 
 if __name__ == "__main__":
-    test(model_path=sys.argv[2], datadir=sys.argv[1], outfilepath=sys.argv[3])
+    test(model_path=sys.argv[2], datadir=sys.argv[1], outfilepath=sys.argv[3], peer=sys.argv[4])
 
