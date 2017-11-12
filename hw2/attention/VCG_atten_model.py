4943e5db2c2b9f7a4d775374f706aa6430fd7c48
haochunchang <b03b02014@ntu.edu.tw>
2017-11-12 16:07:21 +0800

    new attention, test for gru

--- hw2/attention/VCG_atten_model.py
+++ hw2/attention/VCG_atten_model.py
@@ -1,104 +1,88 @@
 import tensorflow as tf
-import numpy as np
+from tensorflow.contrib.layers.python.layers import layers
+import math
 
 class Video_Caption_Generator():
-    def __init__(self, dim_image, n_words, dim_hidden, batch_size, n_lstm_steps, n_video_lstm_step, n_caption_lstm_step, bias_init_vector=None):
-        self.dim_image = dim_image
+    def __init__(self, batch_size, n_words, dim_hidden, dim_image, n_video_lstm_step, n_caption_lstm_step, bias_init_vector=None):
+        self.batch_size = batch_size
         self.n_words = n_words
         self.dim_hidden = dim_hidden
-        self.batch_size = batch_size
-        self.n_lstm_steps = n_lstm_steps
+        self.dim_image = dim_image
         self.n_video_lstm_step = n_video_lstm_step
         self.n_caption_lstm_step = n_caption_lstm_step
-    
+        self.bias_init_vector = bias_init_vector
+
         with tf.device("/cpu:0"):
-            self.Wemb = tf.Variable(tf.random_uniform([n_words, dim_hidden], -0.1, 0.1), name='Wemb')
+            self.Wemb = tf.Variable(tf.truncated_normal([n_words, dim_hidden], stddev=6/math.sqrt(dim_hidden)), name='Wemb')
+
+        self.embed_word_W = tf.Variable(tf.truncated_normal([dim_hidden, n_words], -0.1, 0.1), name='embed_word_W')
+        if bias_init_vector is not None:
+            self.embed_word_b = tf.Variable(bias_init_vector, dtype=tf.float32, name='embed_word_b')
+        else:
+            self.embed_word_b = tf.Variable(tf.zeros([n_words]), name='embed_word_b')
 
         self.lstm1 = tf.contrib.rnn.BasicLSTMCell(dim_hidden, state_is_tuple=True)
         self.lstm2 = tf.contrib.rnn.BasicLSTMCell(dim_hidden, state_is_tuple=True)
 
-        self.encode_image_W = tf.Variable(tf.random_uniform([dim_image, dim_hidden], -0.1, 0.1), name='encode_image_W')
+        self.encode_image_W = tf.Variable(tf.truncated_normal([dim_image, dim_hidden], -0.1, 0.1), name='encode_image_W')
         self.encode_image_b = tf.Variable(tf.zeros([dim_hidden]), name='encode_image_b')
-        
-        # Attention matrix parameters
-        self.embed_att_w = tf.Variable(tf.random_uniform([dim_hidden, 1], -0.1,0.1), name='embed_att_w')
-        self.embed_att_b = tf.Variable( tf.zeros([1]), name='embed_att_b')
-        self.embed_att_Wa = tf.Variable(tf.random_uniform([dim_hidden, dim_hidden], -0.1,0.1), name='embed_att_Wa')
-        self.embed_att_ba = tf.Variable( tf.zeros([dim_hidden]), name='embed_att_ba')
-        
-        self.embed_word_W = tf.Variable(tf.random_uniform([dim_hidden, n_words], -0.1,0.1), name='embed_word_W')
-        if bias_init_vector is not None:
-            self.embed_word_b = tf.Variable(bias_init_vector.astype(np.float32), name='embed_word_b')
-        else:
-            self.embed_word_b = tf.Variable(tf.zeros([n_words]), name='embed_word_b')
+
+        self.attention_W = tf.Variable(tf.truncated_normal([n_video_lstm_step, n_video_lstm_step], -0.01, 0.01), name='attention_W')
+        self.attention_b = tf.Variable(tf.zeros([n_video_lstm_step]), name='attention_b')
 
     def build_model(self):
-        video = tf.placeholder(tf.float32, [self.batch_size, self.n_video_lstm_step, self.dim_image])
+        video = tf.placeholder(tf.float32, [self.batch_size, self.n_video_lstm_step, self.dim_image]) # b x n x d
         video_mask = tf.placeholder(tf.float32, [self.batch_size, self.n_video_lstm_step])
+        video_flat = tf.reshape(video, [-1, self.dim_image]) # (b x n) x d
+
+        image_emb = tf.nn.xw_plus_b(video_flat, self.encode_image_W, self.encode_image_b) # (b x n) x d
+        image_emb = tf.reshape(image_emb, [self.batch_size, self.n_video_lstm_step, self.dim_hidden]) # b x n x d
 
         caption = tf.placeholder(tf.int32, [self.batch_size, self.n_caption_lstm_step+1])
         caption_mask = tf.placeholder(tf.float32, [self.batch_size, self.n_caption_lstm_step+1])
 
-        video_flat = tf.reshape(video, [-1, self.dim_image])
-        image_emb = tf.nn.xw_plus_b( video_flat, self.encode_image_W, self.encode_image_b ) # (batch_size*n_lstm_steps, dim_hidden)
-        image_emb = tf.reshape(image_emb, [self.batch_size, self.n_lstm_steps, self.dim_hidden]) # (b x n x h)
-
-        c_state1, m_state1 = (tf.zeros([self.batch_size, self.lstm1.state_size[0]]), 
-                                tf.zeros([self.batch_size, self.lstm1.state_size[1]]))
-        c_state2, m_state2 = (tf.zeros([self.batch_size, self.lstm2.state_size[0]]), 
-                                tf.zeros([self.batch_size, self.lstm2.state_size[1]]))
+        state1 = self.lstm1.zero_state(self.batch_size, dtype=tf.float32)
+        state2 = self.lstm2.zero_state(self.batch_size, dtype=tf.float32)
         padding = tf.zeros([self.batch_size, self.dim_hidden])
-        h_prev = tf.zeros([self.batch_size, 1, self.dim_hidden])
 
         probs = []
         loss = 0.0
 
-        ######  Encoding Stage ############
+        ##############################  Encoding Stage ##################################
         for i in range(0, self.n_video_lstm_step):
+            if i > 0:
+                tf.get_variable_scope().reuse_variables()
+
             with tf.variable_scope("LSTM1"):
-                if i > 0:
-                   tf.get_variable_scope().reuse_variables()
-                output1, (c_state1, m_state1) = self.lstm1(image_emb[:,i,:], (c_state1, m_state1))
+                output1, state1 = self.lstm1(tf.concat([padding, image_emb[:, i, :]], 1), state1)
 
-            h_prev = tf.concat([h_prev, tf.expand_dims(output1, axis=1)], axis=1)
             with tf.variable_scope("LSTM2"):
-                if i > 0:
-                    tf.get_variable_scope().reuse_variables()
-
-                output2, (c_state2, m_state2) = self.lstm2(tf.concat([padding, output1], 1), (c_state2, m_state2))
-        h_prev = h_prev[:, 1:, :]
-        encoded = tf.transpose(h_prev, [1,0,2]) # from (b x n x h) to (n x b x h)
-        ####### Decoding Stage #############
-        for i in range(0, self.n_caption_lstm_step): 
-            ## Phase 2 => only generate captions
+                output2, state2 = self.lstm2(tf.concat([padding, output1], 1), state2)
+            output2 = tf.reshape(output2, [self.batch_size, self.dim_hidden, 1])
+            
+            # Store hidden state output during encoding stage
             if i == 0:
-                current_embed = tf.zeros([self.batch_size, self.dim_hidden])
+                h_prev = output2
             else:
-                with tf.device("/cpu:0"):
-                    current_embed = tf.nn.embedding_lookup(self.Wemb, caption[:, i]) # Scheduled Sampling can add here
+                h_prev = tf.concat([h_prev, output2], 2) # b x h x n
 
-            # output1: Information about all frames encoded
-            #with tf.variable_scope("LSTM1"):
-            #    tf.get_variable_scope().reuse_variables()
-            #    output1, c_state1, m_state1 = self.lstm1(padding, (c_state1, m_state1))
+        h_prev = tf.reshape(h_prev, [-1, self.n_video_lstm_step]) # (b x h) x n
+        attention = tf.nn.xw_plus_b(h_prev, self.attention_W, self.attention_b) # (b x h) x n
+        attention = tf.reshape(attention, [self.batch_size, self.dim_hidden, self.n_video_lstm_step]) # b x h x n
+        attention = tf.reduce_sum(attention, 2) # b x h
 
-            # current_embed --> (attention matrix) --> attention vector
-            # [ embed(n x b x h) + encoded(n x b x h)] tanh((b x n x h) * (h x 1) + bias(b x n x 1)) = (b x n x 1) --> alphas
-            embeds = tf.tile(tf.expand_dims(current_embed, axis=0), [self.n_lstm_steps, 1, 1])
-            added = tf.concat([embeds, encoded], axis=0) # (2n, b, h)
-            added = tf.transpose(added, [1,0,2]) # (b, 2n, h)
-            added_flat = tf.reshape(added, [-1, self.dim_hidden]) # (b*2n, h)
-            
-            alphas_hidden = tf.tanh(tf.nn.xw_plus_b(added_flat, self.embed_att_Wa, self.embed_att_ba, name='attention_hidden'))# (b*2n, h)
-            alphas = tf.tanh(tf.nn.xw_plus_b(alphas_hidden, self.embed_att_w, self.embed_att_b, name='attention_out')) # (b*2n, 1)
-            alphas = tf.reshape(alphas, [self.batch_size, self.n_video_lstm_step, 2])
-            alphas = tf.reduce_sum(alphas, axis=2, keep_dims=True) # (b, n, 1)
-            atten = tf.reshape(tf.transpose(encoded, [1,2,0]) @ (alphas), [self.batch_size, -1])
-            
-            # Use attention vector to replace output1
+        ############################# Decoding Stage ######################################
+        for i in range(0, self.n_caption_lstm_step):
+            with tf.device("/cpu:0"):
+                current_embed = tf.nn.embedding_lookup(self.Wemb, caption[:, i])
+
+            with tf.variable_scope("LSTM1"):
+                tf.get_variable_scope().reuse_variables()
+                output1, state1 = self.lstm1(tf.concat([attention, padding], 1), state1)
+
             with tf.variable_scope("LSTM2"):
                 tf.get_variable_scope().reuse_variables()
-                output2, (c_state2, m_state2) = self.lstm2(tf.concat([current_embed, atten], 1), (c_state2, m_state2))
+                output2, state2 = self.lstm2(tf.concat([current_embed, output1], 1), state2)
 
             labels = tf.expand_dims(caption[:, i+1], 1)
             indices = tf.expand_dims(tf.range(0, self.batch_size, 1), 1)
@@ -123,68 +107,63 @@ class Video_Caption_Generator():
         image_emb = tf.nn.xw_plus_b(video_flat, self.encode_image_W, self.encode_image_b)
         image_emb = tf.reshape(image_emb, [1, self.n_video_lstm_step, self.dim_hidden])
 
-        c_state1, m_state1 = (tf.zeros([1, self.lstm1.state_size[0]]), 
-                                tf.zeros([1, self.lstm1.state_size[1]]))
-        c_state2, m_state2 = (tf.zeros([1, self.lstm2.state_size[0]]), 
-                                tf.zeros([1, self.lstm2.state_size[1]]))
+        state1 = self.lstm1.zero_state(1, dtype=tf.float32)
+        state2 = self.lstm2.zero_state(1, dtype=tf.float32)
         padding = tf.zeros([1, self.dim_hidden])
-        h_prev = tf.zeros([1, 1, self.dim_hidden])
 
-        generated_words = []
+        video_caption = []
 
         probs = []
         embeds = []
 
+        ##############################  Encoding Stage ##################################
         for i in range(0, self.n_video_lstm_step):
             with tf.variable_scope("LSTM1"):
                 if i > 0:
-                   tf.get_variable_scope().reuse_variables()
-                output1, (c_state1, m_state1) = self.lstm1(image_emb[:,i,:], (c_state1, m_state1))
+                    tf.get_variable_scope().reuse_variables()
+                output1, state1 = self.lstm1(tf.concat([padding, image_emb[:, i, :]], 1), state1)
 
-            h_prev = tf.concat([h_prev, tf.expand_dims(output1, axis=1)], axis=1)
             with tf.variable_scope("LSTM2"):
                 if i > 0:
                     tf.get_variable_scope().reuse_variables()
+                output2, state2 = self.lstm2(tf.concat([padding, output1], 1), state2)
+            output2 = tf.reshape(output2, [1, self.dim_hidden, 1])
+             
+            if i == 0:
+                attention_X = output2
+            else:
+                attention_X = tf.concat([attention_X, output2], 2) # 1 x h x n
 
-                output2, (c_state2, m_state2) = self.lstm2(tf.concat([padding, output1], 1), (c_state2, m_state2))
-        h_prev = h_prev[:, 1:, :]
-        encoded = tf.transpose(h_prev, [1,0,2]) # from (b x n x h) to (n x b x h)
+            attention_X = tf.reshape(attention_X, [-1, self.n_video_lstm_step]) # (1 x h) x n
+            attention = tf.nn.xw_plus_b(attention_X, self.attention_W, self.attention_b) # (1 x h) x n
+            attention = tf.reshape(attention, [1, self.dim_hidden, self.n_video_lstm_step]) # 1 x h x n
+            attention = tf.reduce_sum(attention, 2) # 1 x h
+
+        ############################# Decoding Stage ######################################
         for i in range(0, self.n_caption_lstm_step):
 
-            if i == 0:
-                with tf.device('/cpu:0'):
-                    current_embed = tf.nn.embedding_lookup(self.Wemb, tf.ones([1], dtype=tf.int64))
+        if i == 0:
+            with tf.device('/cpu:0'):
+                current_embed = tf.nn.embedding_lookup(self.Wemb, tf.ones([1], dtype=tf.int64))
 
-            #with tf.variable_scope("LSTM1"):
-            #    tf.get_variable_scope().reuse_variables()
-            #    output1, state1 = self.lstm1(padding, state1)
-            # current_embed --> (attention matrix) --> attention vector
-            # [ embed(n x b x h) + encoded(n x b x h)] tanh((b x n x h) * (h x 1) + bias(b x n x 1)) = (b x n x 1) --> alphas
-            new_embeds = tf.tile(tf.expand_dims(current_embed, axis=0), [self.n_lstm_steps, 1, 1])
-            added = tf.concat([new_embeds, encoded], axis=0) # (2n, b, h)
-            added = tf.transpose(added, [1,0,2]) # (b, 2n, h)
-            added_flat = tf.reshape(added, [-1, self.dim_hidden]) # (b*2n, h)
+        with tf.variable_scope("LSTM1"):
+            tf.get_variable_scope().reuse_variables()
+            output1, state1 = self.lstm1(tf.concat([attention, padding], 1), state1)
 
-            alphas_hidden = tf.tanh(tf.nn.xw_plus_b(added_flat, self.embed_att_Wa, self.embed_att_ba, name='attention_hidden'))# (b*2n, h)
-            alphas = tf.tanh(tf.nn.xw_plus_b(alphas_hidden, self.embed_att_w, self.embed_att_b, name='attention_out')) # (b*2n, 1)
-            alphas = tf.reshape(alphas, [1, self.n_video_lstm_step, 2])
-            alphas = tf.reduce_sum(alphas, axis=2, keep_dims=True) # (b, n, 1)
-            
-            atten = tf.reshape(tf.transpose(encoded, [1,2,0]) @ (alphas), [1, -1]) 
-            with tf.variable_scope("LSTM2"):
-                tf.get_variable_scope().reuse_variables()
-                output2, (c_state2, m_state2) = self.lstm2(tf.concat([current_embed, atten], 1), (c_state2, m_state2))
+        with tf.variable_scope("LSTM2"):
+            tf.get_variable_scope().reuse_variables()
+            output2, state2 = self.lstm2(tf.concat([current_embed, output1], 1), state2)
 
-            logit_words = tf.nn.xw_plus_b(output2, self.embed_word_W, self.embed_word_b)
-            max_prob_index = tf.argmax(logit_words, 1)[0]
-            generated_words.append(max_prob_index)
-            probs.append(logit_words)
+        logit_words = tf.nn.xw_plus_b(output2, self.embed_word_W, self.embed_word_b)
+        max_prob_index = tf.argmax(logit_words, 1)[0]
+        video_caption.append(max_prob_index)
+        probs.append(logit_words)
 
-            with tf.device("/cpu:0"):
-                current_embed = tf.nn.embedding_lookup(self.Wemb, max_prob_index)
-                current_embed = tf.expand_dims(current_embed, 0)
+        with tf.device("/cpu:0"):
+            current_embed = tf.nn.embedding_lookup(self.Wemb, max_prob_index)
+        current_embed = tf.expand_dims(current_embed, 0)
 
-            embeds.append(current_embed)
+        embeds.append(current_embed)
 
-        return video, video_mask, generated_words, probs, embeds
+        return video, video_mask, video_caption, probs, embeds
 
