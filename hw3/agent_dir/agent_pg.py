c6b2bbd86cc6c3944f1f1fad6beea7df9cc86805
haochunchang <b03b02014@ntu.edu.tw>
2017-11-30 23:55:16 +0800

    first

---
+++ hw3/agent_dir/agent_pg.py
@@ -0,0 +1,191 @@
+from agent_dir.agent import Agent
+import tensorflow as tf
+import numpy as np
+import pickle, os
+
+class Agent_PG(Agent):
+    def __init__(self, env, args):
+        """
+        Initialize every things you need here.
+        For example: building your model
+        """
+
+        super(Agent_PG,self).__init__(env)
+        self.env = env
+
+        # Define Agent Model...
+        # Hyper-parameters
+        self.lr = args.lr # learning rate
+        self.bz = args.bz # batch size
+        self.episodes = args.eps # total episodes(epochs)
+        self.gamma = args.gamma
+        self.freq = args.freq
+
+        self.action_size = env.get_action_space().n
+        self.hidden_dim = 200
+
+        self.model = tf.Graph()
+        with self.model.as_default():
+            # Network Architecture
+            self.state_in = tf.placeholder(shape=[None, 210, 160, 3], dtype=tf.float32, name='state_in')
+            
+            # Standardization
+            #red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=self.state_in)
+            #self.state_in = tf.concat(axis=3, values=[ blue - tf.reduce_mean(blue, axis=(1,2)),
+                                                        #green - tf.reduce_mean(green, axis=(1,2)),
+                                                        #red - tf.reduce_mean(red, axis=(1,2))])
+            self.gray_in = tf.image.rgb_to_grayscale(self.state_in)
+            init = tf.contrib.layers.xavier_initializer()
+            self.resized_in = tf.layers.average_pooling2d(self.gray_in, 2, strides=2, padding='valid') # Downsample
+            self.conv = tf.layers.conv2d(self.resized_in, 4, kernel_size=2, padding='same', kernel_initializer=init, activation=tf.nn.relu) 
+            self.maxpool = tf.layers.average_pooling2d(self.conv, 2, strides=2, padding='valid')
+            self.conv = tf.layers.conv2d(self.maxpool, 4, kernel_size=2, padding='same', kernel_initializer=init, activation=tf.nn.relu)
+            self.maxpool = tf.layers.average_pooling2d(self.conv, 2, strides=2, padding='valid')
+            #print(self.maxpool.get_shape())
+            self.hidden = tf.contrib.layers.flatten(self.maxpool)
+            self.hidden = tf.layers.dense(self.hidden, self.hidden_dim, kernel_initializer=tf.contrib.layers.xavier_initializer(), 
+                                            activation=tf.nn.relu)
+            self.hidden2 = tf.layers.dense(self.hidden, self.hidden_dim, kernel_initializer=tf.contrib.layers.xavier_initializer(), 
+                                            activation=tf.nn.relu) 
+            self.output = tf.layers.dense(self.hidden2, self.action_size, kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                                            activation=tf.nn.softmax)
+            #self.chosen_action = tf.argmax(self.output, 1)
+
+            self.reward_holder = tf.placeholder(shape=[None], dtype=tf.float32, name='reward')
+            self.action_holder = tf.placeholder(shape=[None], dtype=tf.int32, name='action')
+
+            # Get action indexes
+            self.indexes = tf.range(0, tf.shape(self.output)[0]) * tf.shape(self.output)[1] + self.action_holder
+            self.responsible_outputs = tf.gather(tf.reshape(self.output, [-1]), self.indexes)
+            self.loss = -tf.reduce_sum(tf.log(self.responsible_outputs)*self.reward_holder)
+        
+            tvars = tf.trainable_variables()
+            self.gradient_holders = []
+            for idx, var in enumerate(tvars):
+                placeholder = tf.placeholder(tf.float32, name=str(idx)+'_holder')
+                self.gradient_holders.append(placeholder)
+
+            self.gradients = tf.gradients(self.loss, tvars)
+            optimizer = tf.train.AdamOptimizer(learning_rate=self.lr)
+            self.update_batch = optimizer.apply_gradients(zip(self.gradient_holders, tvars))
+        
+            if args.test_pg: 
+                model_path = os.path.join('models', 'pg-30')
+            
+                print('loading trained model from {}'.format(model_path))
+                self.sess = tf.InteractiveSession()
+                self.saver = tf.train.Saver()
+                self.saver.restore(self.sess, model_path)
+
+    def init_game_setting(self):
+        """
+
+        Testing function will call this function at the begining of new game
+        Put anything you want to initialize if necessary
+
+        """
+        pass
+
+    def discount_rewards(self, r):
+        """ take 1D float array of rewards and compute discounted reward """
+        discounted_r = np.zeros_like(r)
+        running_add = 0
+        for t in reversed(range(0, r.size)):
+            if r[t] != 0:
+                running_add = 0 # Pong-specific
+            running_add = running_add * self.gamma + r[t]
+            discounted_r[t] = running_add
+        
+        discounted_r = (discounted_r - discounted_r.mean()) / discounted_r.std()
+        return discounted_r
+
+
+    def train(self):
+        """
+        Implement your training algorithm here
+        """
+        # Launch session
+        with tf.Session(graph=self.model) as sess:
+            saver = tf.train.Saver()
+            init = tf.global_variables_initializer()
+            sess.run(init)
+            i = 0
+            total_reward = []
+            total_length = []
+            self.total_r_per_eps = [] # for plotting learning curve
+
+            gradBuffer = sess.run(tf.trainable_variables())
+            for ix, grad in enumerate(gradBuffer):
+                gradBuffer[ix] = grad * 0
+
+            while i < self.episodes:
+                s = (self.env).reset()
+                running_reward = 0
+                episode_his = []
+                done = False
+                while not done:
+                    action_dist = sess.run(self.output, 
+                                            feed_dict={self.state_in: [s]}) 
+                    action = np.random.choice(np.arange(0, action_dist[0].shape[0]), p=action_dist[0])
+                    print(action_dist[0], action)
+                    #action = np.argmax(action_dist == action)
+                    s1, r, done, _ = (self.env).step(action) # Get reward for taking action
+
+                    episode_his.append([s, action, r])
+                    s = s1 - s
+                    running_reward += r
+                    if done: # Update policy network
+                        episode_his = np.array(episode_his)
+                        episode_his[:,2] = self.discount_rewards(episode_his[:,2])
+                        
+                        feed_dict={self.reward_holder: episode_his[:,2],
+                                    self.action_holder: episode_his[:,1],
+                                    self.state_in: np.array([i for i in episode_his[:,0]])}
+                        grads = sess.run(self.gradients, feed_dict=feed_dict)
+                        for idx, grad in enumerate(grads):
+                            gradBuffer[idx] += grad
+                        
+                        if i % self.freq == 0 and i != 0: 
+                            feed_dict = dictionary = dict(zip(self.gradient_holders, gradBuffer))
+                            _ = sess.run(self.update_batch, feed_dict=feed_dict)
+                            for ix, grad in enumerate(gradBuffer):
+                                gradBuffer[ix] = grad * 0
+
+                        total_length.append(episode_his[:,2].shape[0])
+                        total_reward.append(running_reward)
+                        break
+                # Update running tally of rewards
+                if i % 1 == 0:
+                    print(np.mean(total_reward))
+                    #print(total_length)
+                    self.total_r_per_eps.append(total_reward)
+                
+                # save model every k epochs
+                epoch = i
+                if np.mod(epoch, 100) == 0:
+                    print("Saving the model of epoch{}...".format(epoch))
+                    saver.save(sess, './models', global_step=epoch)
+                    with open('total_reward.pkl', 'wb') as p:
+                        pickle.dump(self.total_r_per_eps, p)
+                i += 1
+
+    def make_action(self, observation, test=True):
+        """
+        Return predicted action of your agent
+
+        Input:
+            observation: np.array
+                current RGB screen of game, shape: (210, 160, 3)
+
+        Return:
+            action: int
+                the predicted action from trained model
+        """
+
+        action_dist = self.sess.run(self.output, 
+                                feed_dict={self.state_in: [observation]})
+        action = np.argmax(action_dist)
+
+        return action#self.env.get_random_action()
+
+
---
+++ hw3/argument.py
@@ -0,0 +1,16 @@
+def add_arguments(parser):
+    '''
+    Add your arguments here if needed. The TAs will run test.py to load
+    your default arguments.
+
+    For example:
+        parser.add_argument('--batch_size', type=int, default=32, help='batch size for training')
+        parser.add_argument('--learning_rate', type=float, default=0.01, help='learning rate for training')
+    '''
+    parser.add_argument('-bz', type=int, default=32, help='batch size for training')
+    parser.add_argument('-lr', type=float, default=0.01, help='learning rate for training')
+    parser.add_argument('-eps', type=int, default=8000, help='total learing episodes')
+    parser.add_argument('--gamma', type=float, default=0.99, help='discount factor for rewards')
+    parser.add_argument('--freq', type=float, default=5, help='update frequency')
+  
+    return parser
