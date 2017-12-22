e5af7de5c0e3c964aa8a4bc2a45bb3184daada26
haochunchang <b03b02014@ntu.edu.tw>
2017-12-22 21:30:39 +0800

    add download

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,2 +1,14 @@
 #bash run.sh [testing_text.txt]
-python3 generate.py
+# Download pre-trained models
+if [ ! -d "./models" ]; then
+  mkdir ./models
+  wget --output-document=./models/latest_model.ckpt.data-00000-of-00001 https://gitlab.com/haochunchang/ADLxMLDS2017_hw4_GAN_model/raw/master/latest_model.ckpt.data-00000-of-00001?private_token=sj1NsPumWGUyMvJ6qkH2
+  wget --output-document=./models/latest_model.ckpt.index https://gitlab.com/haochunchang/ADLxMLDS2017_hw4_GAN_model/raw/master/latest_model.ckpt.index?private_token=sj1NsPumWGUyMvJ6qkH2
+  wget --output-document=./models/latest_model.ckpt.meta https://gitlab.com/haochunchang/ADLxMLDS2017_hw4_GAN_model/raw/master/latest_model.ckpt.meta?private_token=sj1NsPumWGUyMvJ6qkH2
+  wget "http://download.tensorflow.org/models/skip_thoughts_uni_2017_02_02.tar.gz"
+  tar -xf skip_thoughts_uni_2017_02_02.tar.gz
+  rm skip_thoughts_uni_2017_02_02.tar.gz
+  mv skip_thoughts_uni_2017_02_02/* ./skip_thoughts/pretrained/
+
+fi
+python3.6 generate.py $1
